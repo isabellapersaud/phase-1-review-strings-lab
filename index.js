688ff5d630703b8ec2a4ff36ee6d035d20475c76
Isabella Persaud <isabellapersaud041600@gmail.com>
2023-01-09 21:04:44 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Sir Cuddle Bucket';
+const welcomeMessage = 'Welcome to Flatbook, ${Sir Cuddle Bucket}!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${SIR CUDDLE BUCKET}!';
+const shortGreeting = 'Welcome, ${S}!';
